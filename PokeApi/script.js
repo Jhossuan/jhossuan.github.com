7b59cb5d1a0433b3f0d13deef6f033209e46ba3a
Jhossuan <jhossuacamposd86@gmail.com>
2022-03-07 12:26:28 -0500

    Finish

--- PokeApi/script.js
+++ PokeApi/script.js
@@ -3,7 +3,7 @@ const cards = d.querySelector(".cards");
 const form = d.querySelector('.form');
 
 function fetchPokemon(name){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)
     .then((res)=> res.json())
     .then((data) => addPokemon(data))
     .catch(err => notFound())
@@ -19,27 +19,38 @@ function addPokemon(data){
     img.setAttribute("src",`${data.sprites.front_default}`)
     const exp = d.querySelector('.exp');
     exp.textContent = `Exp ${data.base_experience}`
+
+    d.querySelector('.error').classList.add('is-active-error')
 }
 
 function notFound(){
-    const name = d.querySelector('.name');
-    name.textContent = "ERROR";
-    const id = d.querySelector('.idx');
-    id.textContent = `ERROR`
-    const img = d.querySelector('.pokeimg');
-    img.setAttribute("src","./imagenes/pokeball.png")
-    const exp = d.querySelector('.exp');
-    exp.textContent = `ERROR`
+    d.querySelector('.card').classList.add('is-active')
+    d.querySelector('.error').classList.remove('is-active-error')
+    const spanerror = d.querySelector('.error-span');
+    spanerror.textContent = 'Pokémon NO encontrado'
 }
+function notFoundInput(){
+    d.querySelector('.card').classList.add('is-active')
+    d.querySelector('.error').classList.remove('is-active-error')
+    const spanerror = d.querySelector('.error-span');
+    spanerror.textContent = 'NOT Found'
+}
+
+d.addEventListener('DOMContentLoaded',()=>{
+    d.querySelector('.error').classList.add('is-active-error')
+})
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
     const input = d.querySelector(".search").value
     if(input === ""){
-        notFound()
+        notFoundInput()
         console.log('Ejecutandose...')
     }else{
         fetchPokemon(input)
+        d.querySelector('.card').classList.remove('is-active')
     }
 })
+
+
 
